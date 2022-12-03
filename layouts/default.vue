40a915e91c8fb0044e1eb52c840bd51ab831d537
Sietse Veenman <s.veenman@gmail.com>
2022-12-03 12:48:42 +0100

    Chore: base css

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,3 +2,31 @@
     <span>Layout</span>
     <slot />
 </template>
+
+<style lang="scss">
+    :root {
+        --font-read: 'Roboto Mono', monospace;
+    }
+    html {
+        font-family: var(--font-read);
+        letter-spacing: 0.025em;
+        font-size: 62.5%;
+        -ms-text-size-adjust: 100%;
+        -webkit-text-size-adjust: 100%;
+        -moz-osx-font-smoothing: grayscale;
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body {
+        font-size: 1.6rem;
+        overscroll-behavior: contain;
+    }
+
+    *,
+    *::before,
+    *::after {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,16 @@
 export default defineNuxtConfig({
   modules: [
     '@pinia/nuxt',
+    
   ],
+  buildModules: [
+    '@nuxtjs/google-fonts'
+  ],
+  
+  googleFonts: {
+    families: {
+      'Roboto+Mono': true,
+    },
+    prefetch: true
+  }
 })
