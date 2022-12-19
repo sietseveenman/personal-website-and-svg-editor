63cf7fa5fcff69ea08ad94eb1ae90ec6033def79
Sietse Veenman <s.veenman@gmail.com>
2022-12-19 01:00:13 +0100

    chore: bounce

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,22 +12,24 @@ export default defineNuxtConfig({
     '@vueuse/nuxt',
   ],
 
-  buildModules: [
-    '@nuxtjs/google-fonts'
-  ],
-
-  googleFonts: {
-    families: { 'Roboto+Mono': true  },
-    prefetch: true
-  },
-
   app: {
     rootId: '__cake', // replacing __nuxt id on root element
     head: {
       charset: 'UTF-8',
       viewport: 'width=device-width, initial-scale=1, minimum-scale=1',
       title: '<SietseVeenman />',
-      description:`"{ "name": "Sietse Veenman", "gender": "Male", "age": 36, "job": "Developer" }"`
+      description:`"{ "name": "Sietse Veenman", "gender": "Male", "age": 36, "job": "Developer" }"`,
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap',
+          crossorigin: ''
+        }
+      ]
     }
   }
 })
