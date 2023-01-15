19c9e14ec71b239114174cb97a33fd1030ec9024
Sietse Veenman <s.veenman@gmail.com>
2023-01-15 09:51:05 +0100

    chore: bounding rect for lines

--- components/Layers/Line.vue
+++ components/Layers/Line.vue
@@ -1,15 +1,20 @@
 <template>
     <g :transform="`translate(${path.position.x}, ${path.position.y})`" fill="transparent">
-       <g class="shape">
-            <path :d="`
-                    M ${path.a2.x} ${path.a2.y} 
-                    C ${path.c2.x} ${path.c2.y} ${path.c1.x} ${path.c1.y} ${path.a1.x} ${path.a1.y}
+
+        <path class="shape" ref="line"
+            :d="`
+                M ${path.a2.x} ${path.a2.y} 
+                C ${path.c2.x} ${path.c2.y} ${path.c1.x} ${path.c1.y} ${path.a1.x} ${path.a1.y}
                 `" 
-                :stroke="lineColor"
-                :stroke-dasharray="dashArray"
-                :stroke-dashoffset="dashOffset"
-                ref="line"/>
-        </g>
+            :stroke="lineColor"
+            :stroke-dasharray="dashArray"
+            :stroke-dashoffset="dashOffset" />
+            
+        <rect fill="transparent" v-if="gotRect" @click="test"
+            stroke-dasharray="8" stroke="var(--c-two)" :stroke-opacity="0.4"
+            :width="clickabelRectSize.width" :height="clickabelRectSize.height" 
+            :x="clickabelRectSize.x" :y="clickabelRectSize.y" />
+    
         <circle class="handle" id="c1"
             @mousedown.prevent="(e)=>appState.setActiveAnchor(e,pathName, 'c1')"
             @touchstart.prevent="(e)=>appState.setActiveAnchor(e,pathName, 'c1')"
@@ -29,8 +34,8 @@
             :cx="path.c2.x" :cy="path.c2.y" />
     
         <rect class="handle" id="a1"    
-        :width="rectSize" :height="rectSize"  
-            :x="path.a1.x - (rectSize/2)" :y="path.a1.y - (rectSize/2)" 
+        :width="anchorSize" :height="anchorSize"  
+            :x="path.a1.x - (anchorSize/2)" :y="path.a1.y - (anchorSize/2)" 
             :transform="`rotate(45 ${ path.a1.x } ${ path.a1.y })`"
             fill="transparent"
             @mousedown.prevent="(e)=>appState.setActiveAnchor(e,pathName, 'a1')"
@@ -40,8 +45,8 @@
             stroke-width="1"/>
 
         <rect class="handle" id="a2"    
-            :width="rectSize" :height="rectSize"  
-            :x="path.a2.x - (rectSize/2)" :y="path.a2.y - (rectSize/2)" 
+            :width="anchorSize" :height="anchorSize"  
+            :x="path.a2.x - (anchorSize/2)" :y="path.a2.y - (anchorSize/2)" 
             :transform="`rotate(45 ${ path.a2.x } ${ path.a2.y })`"
             @mousedown.prevent="(e)=>appState.setActiveAnchor(e,pathName, 'a2')"
             @touchstart.prevent="(e)=>appState.setActiveAnchor(e,pathName, 'a2')"
@@ -52,14 +57,17 @@
     </g>
 </template>
 
+<style>
+
+</style>
+
 <script setup>
     import { useAppState } from '@/stores/appState'
     import { useBaseLayers } from '@/stores/baseLayers'
-
+    import { storeToRefs } from 'pinia'
+    
     const appState = useAppState()
     const layers = useBaseLayers()
-
-    const rectSize =  computed(  () => appState.windowSize.width < 786 ? 24 : 14 )
 
     const props = defineProps({ 
         pathName: String,
@@ -82,15 +90,51 @@
         6: 'var(--c-six)',
     }
 
-    const dashOffset = ref(0)
-    function loop() {
-        if ( document.hasFocus() ) dashOffset.value += Number(props.speed)
-        requestAnimationFrame(loop)
-    }
-    if( props.dashArray ) loop()
+    const anchorSize =  computed(  () => appState.windowSize.width < 786 ? 24 : 14 )
 
     const path = computed( () => layers[props.pathName] )
     const lineColor = computed( () => palets[props.palet]  )
     const handleColor = computed( () => Number(props.palet) >= 5  ? palets[1] : palets[Number(props.palet)+1]  )
+    
+    const dashOffset = ref(0)
+    const line = ref(null)
+    const gotRect = ref(false)
+    
+    const clickabelRectSize = ref({
+        width:0,
+        height:0,
+        x:0,
+        y:0
+    })
+
+    watch(path, () => {
+        setClickable(line.value.getBoundingClientRect(), path.value.position)
+    }, {deep:true})
+
+
+    onMounted(() => {
+        setClickable(line.value.getBoundingClientRect(), path.value.position)
+        if( props.dashArray ) loop()
+    })
+
+    function test () {
+        console.log('test')
+    }
+
+    function setClickable(rect, pos) {
+        const { width, height, top, left } = rect
+        clickabelRectSize.value = {
+            width,
+            height,
+            x: left - (pos.x - window.scrollX),
+            y: top  - (pos.y - window.scrollY),
+        }
+        gotRect.value = true
+    }
+    
+    function loop() {
+        if ( document.hasFocus() ) dashOffset.value += Number(props.speed)
+        requestAnimationFrame(loop)
+    }
 
 </script>
